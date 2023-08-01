1ece27b015fc755115bf3f9995b3e8153401557e
wangchenghai <1454877124@qq.com>
2023-08-01 17:31:10 +0800

    使用localStorage

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,14 @@
     ></el-autocomplete>
     <el-table
         :data="gz"
-        stripe
+        stripe v-loading="loading"
         :cell-style="{textAlign:'center'}"
         :header-cell-style="{textAlign:'center'}"
         style="width: 100%">
       <el-table-column
           prop="name" show-overflow-tooltip
+          min-width="180"
           label="基金名称">
-      </el-table-column>
-      <el-table-column
-          prop="FCODE"
-          label="基金代码">
       </el-table-column>
       <el-table-column
           prop="gz"
@@ -30,7 +27,19 @@
         <template #default="scope">
           <span style="color: red" v-if="Number(scope.row.gz)>0">+{{ scope.row.gz }}%</span>
           <span style="color: green" v-else-if="Number(scope.row.gz)<0">{{ scope.row.gz }}%</span>
-          <span v-else>{{ scope.row.gz }}%</span>
+          <span v-else>{{ scope.row.gz||'--' }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="FCODE"
+          label="基金代码">
+      </el-table-column>
+      <el-table-column
+          label="操作"
+          fixed="right"
+          width="50">
+        <template #default="scope">
+          <el-button @click="deleteCode(scope)" type="text" size="small">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -46,33 +55,62 @@ export default {
     return {
       founds: [],
       state: '',
+      loading:false,
       fcodes: ['012769','002708','000362','012323','001092','010364','017811','161628'],
       gz: []
     }
   },
   mounted() {
-    this.getGz();
+    this.syncFCodes();
   },
   methods: {
+    deleteCode({$index}){
+      this.fcodes.splice($index,1);
+      this.syncFCodes(this.fcodes);
+    },
+    //同步fcodes
+    syncFCodes(newFCodes){
+      let fcodes = localStorage.getItem('fcodes')
+      if(newFCodes){
+        localStorage.setItem('fcodes',JSON.stringify(newFCodes))
+      }else if(!fcodes){
+        localStorage.setItem('fcodes',JSON.stringify(this.fcodes))
+      }else {
+        fcodes= JSON.parse(fcodes);
+        console.log(fcodes)
+        this.fcodes = fcodes
+      }
+      this.getGz()
+    },
     getGz(){
       const data = {
         fcodes: this.fcodes.toString()
       }
+      this.loading = true;
       axios({
         url: '/found/favor/GetFundsInfo?',
         method: "post",
         data
       }).then((res) => {
-        this.gz = res.data.Data.KFS.map(item => ({name: item.SHORTNAME, gz: item.gszzl,...item}));
+        this.gz = [];
+        this.fcodes.forEach(item=>{
+          const fcode = res.data.Data.KFS.find(_item=>_item.FCODE===item);
+          this.gz.push({name: fcode?.SHORTNAME??'--', gz: fcode?.gszzl??'--',...fcode,FCODE:item})
+        })
       }).catch(function (error) {
         console.log(error);
+      }).finally(()=>{
+        this.loading = false;
       });
     },
     handleSelect(item) {
       if(!this.fcodes.includes(item.CODE)){
         this.fcodes.push(item.CODE);
-        this.getGz();
+        this.syncFCodes(this.fcodes);
       }
+      setTimeout(()=>{
+        this.state = ''
+      })
     },
     querySearchAsync(queryString, cb) {
       const params = {
@@ -110,6 +148,6 @@ export default {
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  padding: 10vh 10vw;
+  padding: 10vh calc(10vw - 50px) 10vh calc(10vw - 30px);
 }
 </style>
