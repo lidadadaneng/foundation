5dc0ac60d8200cf8dacb43c4e27ea6cb5278e409
wangchenghai <1454877124@qq.com>
2023-08-02 14:54:35 +0800

    按需引入

---
+++ src/vue.use.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import {Table, Button, Autocomplete, TableColumn, Loading} from "element-ui";
+
+
+Vue.use(Table);
+Vue.use(Autocomplete);
+Vue.use(Button);
+Vue.use(TableColumn);
+Vue.use(Loading);
